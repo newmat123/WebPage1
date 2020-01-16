45d4f3bdeb6f826239576144c30a871317a528c6
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-01-16 11:18:01 +0100

    starting to work on backend

---
+++ myScript.js
@@ -0,0 +1,8 @@
+var container = document.getElementsByClassName()
+
+function f() {
+  var card = document.createElement("div");
+  var node = document.createTextClass("card");
+  card.appendChild(node);
+  container.appendChild(card);
+}
