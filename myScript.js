fb018e884a1bb584fd1105775dd594ed941aab63
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-01-16 22:41:04 +0100

    working on the option pannel

--- myScript.js
+++ myScript.js
@@ -1,4 +1,40 @@
-function createCard() {
+
+function openOptionPannel() {
+
+  var overlay = document.createElement('div');
+  var pannel = document.createElement('div');
+
+  var imageDiv = [];
+  var images = ['pictures\\Python.png', 'pictures\\javascript.png', 'pictures\\cIcon.png'];
+
+  for (var i = 0; i < images.length; i++) {
+    imageDiv[i] = document.createElement('img');
+  }
+
+
+  overlay.setAttribute('id', 'optionDisplay');
+  pannel.setAttribute('class', 'optionPannel');
+
+
+  for (var i = 0; i < imageDiv.length; i++) {
+    imageDiv[i].setAttribute('class', 'pic');
+    imageDiv[i].setAttribute('src', images[i]);
+    pannel.appendChild(imageDiv[i]);
+  }
+
+  overlay.appendChild(pannel);
+
+  document.body.appendChild(overlay);
+
+
+  createCard("pictures\\Python.png", 'yes', 'text', 'https://github.com/newmat123/Backtraking')
+
+}
+
+
+
+
+function createCard(picture, picText, discript, linkToGit) {
 
   //creates all the div's, text and img needed for the card.
   var cards = document.createElement('div');
@@ -8,36 +44,36 @@ function createCard() {
   var con1 = document.createElement('div');
 
   var img = document.createElement('img');
-  var h3 = document.createElement('h3')
+  var h3 = document.createElement('h3');
 
 
   var face2 = document.createElement('div');
   var con2 = document.createElement('div');
 
   var innerText = document.createElement('p');
-  var link = document.createElement('a')
+  var link = document.createElement('a');
 
 
   //gives the difrent div's class names (used for the style)
-  cards.setAttribute('class', 'card')
+  cards.setAttribute('class', 'card');
 
-  face1.setAttribute('class', 'face face1')
-  con1.setAttribute('class', 'content')
+  face1.setAttribute('class', 'face face1');
+  con1.setAttribute('class', 'content');
 
-  face2.setAttribute('class', 'face face2')
-  con2.setAttribute('class', 'content')
+  face2.setAttribute('class', 'face face2');
+  con2.setAttribute('class', 'content');
 
 
   //setting the img and img-text
-  img.setAttribute('src', 'pictures\\Python.png');
-  h3.innerHTML = 'the text';
+  img.setAttribute('src', picture);
+  h3.innerHTML = picText;
 
   //sets the inner tex
-  innerText.innerHTML = 'fimjruigjdtisfjkgithojmrtgifeihregjijrfikjitgji text'
+  innerText.innerHTML = discript;
 
   //sets the link to the github
-  link.innerHTML = 'Go to github >>'
-  link.setAttribute('href', 'https://github.com/newmat123/Backtraking')
+  link.innerHTML = 'Go to github >>';
+  link.setAttribute('href', linkToGit);
 
 
 
