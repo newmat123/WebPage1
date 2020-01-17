323f0558c95e176ce5418a685c707bf61cbb0970
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-01-17 14:32:35 +0100

    i

--- myScript.js
+++ myScript.js
@@ -1,3 +1,8 @@
+function loadAll() {
+
+}
+
+
 
 function openOptionPannel() {
 
@@ -28,6 +33,8 @@ function collectInfo() {
   var pannel = document.getElementById('optionDisplay')
   pannel.style.display = 'none';
 }
+
+
 
 
 function createCard(picture, picText, discript, linkToGit) {
